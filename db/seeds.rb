e624ad85ca51400472f06c0a5c793cc49b266194
johnhaus <john.haus@outlook.com>
2018-11-13 20:57:07 -0300

    populated db with seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create(name: "Pizza Place", city: "London", food_type: "Italian", rating: 4)
+Restaurant.create(name: "Burger Shack", city: "New York", food_type: "Fast food", rating: 2)
+Restaurant.create(name: "The Wok", city: "Paris", food_type: "Chinese", rating: 5)
+Restaurant.create(name: "Happy Cow", city: "Tokyo", food_type: "Ice Cream", rating: 3)
+Restaurant.create(name: "Wake Up", city: "San Francisco", food_type: "Coffee", rating: 4)
