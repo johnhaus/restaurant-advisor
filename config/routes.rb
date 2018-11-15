0f544f43df8e98803a8bbf24ade9a0ff64f958e9
johnhaus <john.haus@outlook.com>
2018-11-15 20:21:02 -0300

    added 'top restaurants' route/link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get "top", to: "restaurants#top"
+    end
+  end
 
   root to: 'restaurants#index'
 end
