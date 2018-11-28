0a487d357d3764bd9b96491acaacfa9310bd685f
johnhaus <john.haus@outlook.com>
2018-11-28 18:02:36 -0300

    set up unique pics for each restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_235814) do
+ActiveRecord::Schema.define(version: 2018_11_28_201931) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_235814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "chef"
+    t.string "picture"
   end
 
   create_table "reviews", force: :cascade do |t|
