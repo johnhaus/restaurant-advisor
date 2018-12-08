bfd055c247e710b7bc368bbbca722f972dbd8865
johnhaus <john.haus@outlook.com>
2018-12-07 21:49:04 -0300

    added photo of chef upload option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_000228) do
+ActiveRecord::Schema.define(version: 2018_12_08_004501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_12_07_000228) do
     t.string "picture"
     t.string "avatar"
     t.string "photo"
+    t.string "photo_of_the_chef"
   end
 
   create_table "reviews", force: :cascade do |t|
