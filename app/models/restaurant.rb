95f8a33022af97e3829cfdc43d14c07813c32000
johnhaus <john.haus@outlook.com>
2018-12-07 21:30:28 -0300

    modified to allow future photo upload for chef, tweaked input for better appearance

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,7 +4,8 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :city, presence: true
   validates :food_type, presence: true
-  validates :rating, inclusion: { in: [1,2,3,4,5], allow_nil: false}
+  validates :rating, presence: true, inclusion: { in: [1,2,3,4,5], allow_nil: false}
 
   mount_uploader :photo, PhotoUploader
+  mount_uploader :photo_of_the_chef, PhotoUploader
 end
