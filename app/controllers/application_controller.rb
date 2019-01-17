9509948df8b23db3595e5807bb0c9869f66d06db
johnhaus <john.haus@outlook.com>
2019-01-17 17:26:13 -0300

    added an error message for unauthorized users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: [:index, :top, :show], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: [:index, :top, :show], unless: :skip_pundit?
 
-
+  rescue_from Pundit::NotAuthorizedError,   with: :user_not_authorized
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
   # def configure_permitted_parameters
@@ -18,5 +18,11 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller?
+  end
+
+  def user_not_authorized(exception)
+    render json: {
+      error: "Sorry, you are not authorized to perform that action."
+    }, status: :unauthorized
   end
 end
