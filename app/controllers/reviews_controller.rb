454c8c6d0b6fb4a9ead306ef558eeefd27ea6fb5
johnhaus <john.haus@outlook.com>
2019-01-17 18:02:10 -0300

    added confirmation that review was created

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,7 +14,7 @@ class ReviewsController < ApplicationController
     authorize @review
 
     if @review.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_path(@restaurant), notice: 'Review was successfully created.'
     else
       render :new
     end
